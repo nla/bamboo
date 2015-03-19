121706a0843867e4a647926449a1d0a2d65ff8e8
Alex Osborne <aosborne@nla.gov.au>
2015-03-19 15:07:48 +1100

    Set saner default for WARC rollup fields

---
+++ resources/bamboo/migrations/V14__Saner_defaults_for_warc_rollup.sql
@@ -0,0 +1,4 @@
+ALTER TABLE crawl ALTER warc_files SET DEFAULT 0;
+ALTER TABLE crawl ALTER warc_size SET DEFAULT 0;
+ALTER TABLE crawl_series ALTER warc_files SET DEFAULT 0;
+ALTER TABLE crawl_series ALTER warc_size SET DEFAULT 0;
