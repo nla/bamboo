d4396ff0766aa9de15b8416f52be049cf9b742a7
Alex Osborne <aosborne@nla.gov.au>
2016-02-01 08:39:09 +1100

    Create warcs in the imported state by default

--- src/bamboo/crawl/Warc.java
+++ src/bamboo/crawl/Warc.java
@@ -21,8 +21,8 @@ public class Warc {
     private String filename;
     private String sha256;
 
-	public Warc() {
-	}
+    public Warc() {
+    }
 
     public Warc(ResultSet rs) throws SQLException {
         setId(rs.getLong("id"));
@@ -36,84 +36,85 @@ public class Warc {
         setSha256(rs.getString("sha256"));
     }
 
-	public static Warc fromFile(Path path) throws IOException {
+    public static Warc fromFile(Path path) throws IOException {
         Warc warc = new Warc();
         warc.setPath(path);
-		warc.setFilename(path.getFileName().toString());
+        warc.setFilename(path.getFileName().toString());
         warc.setSize(Files.size(path));
         warc.setSha256(Scrub.calculateDigest("SHA-256", path));
+        warc.setStateId(IMPORTED);
         return warc;
     }
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public long getCrawlId() {
-		return crawlId;
-	}
+    public long getCrawlId() {
+        return crawlId;
+    }
 
-	public void setCrawlId(long crawlId) {
-		this.crawlId = crawlId;
-	}
+    public void setCrawlId(long crawlId) {
+        this.crawlId = crawlId;
+    }
 
-	public int getStateId() {
-		return stateId;
-	}
+    public int getStateId() {
+        return stateId;
+    }
 
-	public void setStateId(int stateId) {
-		this.stateId = stateId;
-	}
+    public void setStateId(int stateId) {
+        this.stateId = stateId;
+    }
 
-	public Path getPath() {
-		return path;
-	}
+    public Path getPath() {
+        return path;
+    }
 
-	public void setPath(Path path) {
-		this.path = path;
-	}
+    public void setPath(Path path) {
+        this.path = path;
+    }
 
-	public long getSize() {
-		return size;
-	}
+    public long getSize() {
+        return size;
+    }
 
-	public void setSize(long size) {
-		this.size = size;
-	}
+    public void setSize(long size) {
+        this.size = size;
+    }
 
-	public long getRecords() {
-		return records;
-	}
+    public long getRecords() {
+        return records;
+    }
 
-	public void setRecords(long records) {
-		this.records = records;
-	}
+    public void setRecords(long records) {
+        this.records = records;
+    }
 
-	public long getRecordBytes() {
-		return recordBytes;
-	}
+    public long getRecordBytes() {
+        return recordBytes;
+    }
 
-	public void setRecordBytes(long recordBytes) {
-		this.recordBytes = recordBytes;
-	}
+    public void setRecordBytes(long recordBytes) {
+        this.recordBytes = recordBytes;
+    }
 
-	public String getFilename() {
-		return filename;
-	}
+    public String getFilename() {
+        return filename;
+    }
 
-	public void setFilename(String filename) {
-		this.filename = filename;
-	}
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
 
-	public String getSha256() {
-		return sha256;
-	}
+    public String getSha256() {
+        return sha256;
+    }
 
-	public void setSha256(String sha256) {
-		this.sha256 = sha256;
-	}
+    public void setSha256(String sha256) {
+        this.sha256 = sha256;
+    }
 }
