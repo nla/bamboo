860484a53a696ab614bc506679cbb609a37010f9
Alex Osborne <aosborne@nla.gov.au>
2016-10-31 13:36:05 +1100

    Pandas directory sync

--- ui/src/bamboo/pandas/PandasRestrictions.java
+++ ui/src/bamboo/pandas/PandasRestrictions.java
@@ -1,9 +1,8 @@
 package bamboo.pandas;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import bamboo.core.Config;
+
+import java.util.*;
 
 public class PandasRestrictions {
     PandasDAO dao;
@@ -12,14 +11,63 @@ public class PandasRestrictions {
         this.dao = dao;
     }
 
-    void sync() {
+    void dump() {
         for (PeriodRestr restr : dao.listPeriodRestrictions()) {
             Set<String> urls = urlsForTitle(restr.getTitleId());
-            System.out.println(restr.getPeriodMultiplier() + " * " + restr.getPeriodTypeId() + " " + urls);
+            AccessRule rule = new AccessRule();
+            rule.policy = "block";
+            rule.surt = "au,gov,nla,pandora,)/pan/" + restr.getTitleId() + "/";
+            rule.secondsSinceCapture = restr.getSecondsSinceCapture();
+            rule.privateComment = "Migrated from PANDAS";
+            rule.enabled = true;
+            rule.who = "ip:" + Integer.toString(restr.getAgencyAreaId());
+
+            System.out.println(rule.toString());
         }
     }
 
     private Set<String> urlsForTitle(long titleId) {
         return null;
     }
+
+    static class AccessRule {
+        Long id;
+        String policy;
+        String surt;
+        Date captureStart;
+        Date captureEnd;
+        Date retrievalStart;
+        Date retrievalEnd;
+        Long secondsSinceCapture;
+        String who;
+        String privateComment;
+        String publicComment;
+        Boolean enabled;
+        Boolean exactMatch = Boolean.FALSE;
+
+        @Override
+        public String toString() {
+            return "AccessRule{" +
+                    "id=" + id +
+                    ", policy='" + policy + '\'' +
+                    ", surt='" + surt + '\'' +
+                    ", captureStart=" + captureStart +
+                    ", captureEnd=" + captureEnd +
+                    ", retrievalStart=" + retrievalStart +
+                    ", retrievalEnd=" + retrievalEnd +
+                    ", secondsSinceCapture=" + secondsSinceCapture +
+                    ", who='" + who + '\'' +
+                    ", privateComment='" + privateComment + '\'' +
+                    ", publicComment='" + publicComment + '\'' +
+                    ", enabled=" + enabled +
+                    ", exactMatch=" + exactMatch +
+                    '}';
+        }
+    }
+
+    public static void main(String args[]) {
+        PandasDB db = new PandasDB(new Config());
+        new PandasRestrictions(db.dao).dump();
+    }
+
 }
