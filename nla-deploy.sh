a5e4906bc34b65176a07cda4cecd0ad1f30fccf3
Alex Osborne <aosborne@nla.gov.au>
2017-09-05 16:13:27 +0900

    Ignore sources jar when deploying

--- nla-deploy.sh
+++ nla-deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mvn -pl common,ui package -Ppandas
 cp -v ui/target/bamboo-*.jar $1/
-(cd $1; ln -s bamboo-*.jar bamboo.jar)
+(cd $1; rm -f *-sources.jar; ln -s bamboo-*.jar bamboo.jar)
