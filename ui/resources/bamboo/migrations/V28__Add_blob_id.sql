a73b2a1a573b8ad63d5f1998189c6f44c2ae9d86
Alex Osborne <aosborne@nla.gov.au>
2018-06-26 18:52:21 +0900

    Allow path fields to be nullable now that they could have blob_id

--- ui/resources/bamboo/migrations/V28__Add_blob_id.sql
+++ ui/resources/bamboo/migrations/V28__Add_blob_id.sql
@@ -1,2 +1,6 @@
 ALTER TABLE warc ADD blob_id bigint;
 ALTER TABLE artifact ADD blob_id bigint;
+
+-- make path fields nullable
+ALTER TABLE warc MODIFY path varchar(4096) NULL;
+ALTER TABLE artifact MODIFY path varchar(4096) NULL;
