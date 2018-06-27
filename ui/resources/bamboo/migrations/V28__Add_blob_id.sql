d7245686f0a741cf1ef72e7043b1cec18c7050e4
Alex Osborne <aosborne@nla.gov.au>
2018-06-27 20:47:55 +0900

    Separate path and relpath. Also keep artifact last modified just in case

--- ui/resources/bamboo/migrations/V28__Add_blob_id.sql
+++ ui/resources/bamboo/migrations/V28__Add_blob_id.sql
@@ -1,5 +1,5 @@
 ALTER TABLE warc ADD blob_id bigint;
-ALTER TABLE artifact ADD blob_id bigint;
+ALTER TABLE artifact ADD (blob_id bigint, relpath VARCHAR(255), last_modified TIMESTAMP);
 
 -- make path fields nullable
 ALTER TABLE warc MODIFY path varchar(4096) NULL;
