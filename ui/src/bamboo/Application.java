5583e734488a8662c8d84a9b0037182eebb0f09f
Alex Osborne <aosborne@nla.gov.au>
2019-10-28 18:32:07 +0900

    Remove dead code

--- ui/src/bamboo/Application.java
+++ ui/src/bamboo/Application.java
@@ -21,20 +21,4 @@ public class Application {
     public Bamboo getBamboo() throws IOException {
         return new Bamboo(false);
     }
-
-//    @Bean
-//    public FreeMarkerConfigurer freemarkerConfig() {
-//        FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
-//        freeMarkerConfigurer.setConfiguration(Freemarker.config);
-//        return freeMarkerConfigurer;
-//    }
-//
-//    @Bean
-//    public FreeMarkerViewResolver freemarkerViewResolver() {
-//        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
-//        resolver.setCache(true);
-//        resolver.setPrefix("");
-//        resolver.setSuffix(".ftl");
-//        return resolver;
-//    }
 }
