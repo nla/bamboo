9e23e9554490955251cc5835c0cfc78e4d9db290
Alex Osborne <aosborne@nla.gov.au>
2019-10-29 15:50:41 +0900

    Add command-line options compatible with old main()

--- ui/src/bamboo/Application.java
+++ ui/src/bamboo/Application.java
@@ -10,6 +10,29 @@ import java.io.IOException;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "-h":
+                case "--help":
+                    System.out.println("Options:\n" +
+                            "  -p port  Web server port\n" +
+                            "  -b addr  Web server bind address\n" +
+                            "  -c path  Web server context path");
+                    return;
+                case "-c":
+                    System.setProperty("server.servlet.context-path", args[++i]);
+                    break;
+                case "-p":
+                    System.setProperty("server.port", args[++i]);
+                    break;
+                case "-b":
+                    System.setProperty("server.address", args[++i]);
+                    break;
+                default:
+                    System.err.println("Ignoring unknown option: " + args[i]);
+                    break;
+            }
+        }
         SpringApplication.run(Application.class, args);
     }
 
