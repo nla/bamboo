695722521101d01f1be34e707590937a7008717f
Alex Osborne <aosborne@nla.gov.au>
2020-03-26 15:51:45 +0900

    Enable pagination for Crawl and Warc repositories

--- ui/src/bamboo/crawl/CrawlRepository.java
+++ ui/src/bamboo/crawl/CrawlRepository.java
@@ -1,12 +1,12 @@
 package bamboo.crawl;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface CrawlRepository extends CrudRepository<Crawl, Long> {
+public interface CrawlRepository extends PagingAndSortingRepository<Crawl, Long> {
     Optional<Crawl> findByWebrecorderCollectionId(@Param("webrecorderCollectionId") String webrecorderCollectionId);
 }
--- ui/src/bamboo/crawl/WarcRepository.java
+++ ui/src/bamboo/crawl/WarcRepository.java
@@ -1,10 +1,10 @@
 package bamboo.crawl;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface WarcRepository extends CrudRepository<Warc, Long> {
+public interface WarcRepository extends PagingAndSortingRepository<Warc, Long> {
     Optional<Warc> findByFilename(@Param("filename") String filename);
 }
