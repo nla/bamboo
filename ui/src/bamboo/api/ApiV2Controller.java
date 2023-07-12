fc166854e658c51d1aa0701834ca757b7b6212b4
Alex Osborne <aosborne@nla.gov.au>
2023-07-12 14:43:20 +0900

    Support POST requests on /api/v2/CrawlByWarcFilename
    This is to workaround hitting request size limits when lookup a lot of files in one go.

--- ui/src/bamboo/api/ApiV2Controller.java
+++ ui/src/bamboo/api/ApiV2Controller.java
@@ -2,11 +2,12 @@ package bamboo.api;
 
 import bamboo.app.Bamboo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 public class ApiV2Controller {
@@ -17,7 +18,8 @@ public class ApiV2Controller {
     }
 
     // Used by pandas-admin for the pages feature
-    @GetMapping("/api/v2/CrawlsByWarcFilename")
+    // supports both GET and POST in order to allow larger requests
+    @RequestMapping(value = "/api/v2/CrawlsByWarcFilename", method = {GET, POST})
     @ResponseBody
     public List<ApiDAO.CrawlsByFilename> crawlsByWarcFilename(@RequestParam("filename") List<String> filenames) {
         return wa.dao.api().crawlsByWarcFilename(filenames);
